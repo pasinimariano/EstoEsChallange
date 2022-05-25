7970bd651d9ea6ba45655f9e2c9ba32137067546
pasini_mariano <pasinimariano@gmail.com>
2022-05-25 16:46:21 -0300

    Fixing bugs from cannot get

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath
+    publicPath,
+    filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.jsx']
